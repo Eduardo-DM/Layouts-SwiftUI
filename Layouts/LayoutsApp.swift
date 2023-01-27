689f02dc70f79a8dcac59a08ff9055ecb9297dfe
Eduardo <eduardodmdev@icloud.com>
2023-01-27 16:04:09 +0100

    Initial Commit

---
+++ Layouts/LayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutsApp.swift
+//  Layouts
+//
+//  Created by Eduardo Developer on 27/1/23.
+//
+
+import SwiftUI
+
+@main
+struct LayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
