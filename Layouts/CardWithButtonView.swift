7559e28d662b10a0dc9d986811cd585a309cb718
Eduardo <eduardodmdev@icloud.com>
2023-03-06 13:09:58 +0100

    Start of Card with a button

---
+++ Layouts/CardWithButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  CardWithButtonView.swift
+//  Layouts
+//
+//  Created by Eduardo Developer on 6/3/23.
+//
+
+import SwiftUI
+
+struct CardWithButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardWithButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardWithButtonView()
+    }
+}
