2b1dd26c67ba380eb55249958daccbdc41feb51f
Eduardo <eduardodmdev@icloud.com>
2023-03-06 14:26:10 +0100

    set background in CardWithButtonView

--- Layouts/CardWithButtonView.swift
+++ Layouts/CardWithButtonView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct CardWithButtonView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+        .background{
+            Image("BackgroundDesktop14proMax")
+                .ignoresSafeArea()
+        }
     }
 }
 
